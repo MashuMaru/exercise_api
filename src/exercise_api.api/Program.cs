c11d6dd34cef57ec456d8390b1458d4e02a1477b
Matthew Berenyi <matthew.berenyi@poolesoftware.com>
2022-08-26 16:42:56 +0100

    Connection string SQL Db

--- src/exercise_api.api/Program.cs
+++ src/exercise_api.api/Program.cs
@@ -1,25 +1,42 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.Data.SqlClient;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+internal class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+  private static void Main(string[] args)
+  {
+    var builder = WebApplication.CreateBuilder(args);
+
+    // Add services to the container.
+
+    builder.Services.AddControllers();
+
+    SqlConnectionStringBuilder connectionBuilder = new SqlConnectionStringBuilder();
+    connectionBuilder.DataSource = "<your_server.database.windows.net>"; 
+    connectionBuilder.UserID = "<your_username>";            
+    connectionBuilder.Password = "<your_password>";     
+    connectionBuilder.InitialCatalog = "<your_database>";
+    // OR 
+    connectionBuilder.ConnectionString="<your_ado_net_connection_string>";
+
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+      app.UseSwagger();
+      app.UseSwaggerUI();
+    }
+
+    // app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
+  }
 }
-
-// app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
